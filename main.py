75642d2b570f42d8131c084e3daf67c9f99939aa
Ruslan Zhdan <zhd.ruslan@gmail.com>
2019-07-15 16:49:00 +0300

    all

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        print('GET')
+        return render_template('index.html')
+    if request.method == 'POST':
+        print('POST')
+        return render_template('tst.html')
